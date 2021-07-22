d55e99232a340d2885cd67ccb83f2a8dae4b4b8f
Parthy Goyal <parthg0611@gmail.com>
2021-07-22 16:56:53 -0400

    missed line

--- src/introToScanner.java
+++ src/introToScanner.java
@@ -4,5 +4,12 @@ public class introToScanner {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         System.out.println(N);
+        System.out.println("Finished Changes");
+        System.out.println("This is a change though");
+        helloWorld();
+
+    }
+    public static void helloWorld(){
+        System.out.println("hello world");
     }
 }
