775f689800690f695bc306fb576e192efb97f344
Parthy Goyal <parthg0611@gmail.com>
2022-08-02 20:52:37 -0400

    Update Some Solutions

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    Scanner sc = new Scanner(System.in);
+    String number = sc.nextLine();
+    ArrayList<Character> individuals = new ArrayList<>();
+    ArrayList<Character> reversed = new ArrayList<>();
+
+
+
+
+
+
+}
